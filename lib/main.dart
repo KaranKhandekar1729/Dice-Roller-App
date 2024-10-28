63f986167b0e85621409557a26db6f13d7cc7d10
Karan <karankhandekar028@gmail.com>
2024-10-28 23:43:55 +0530

    Built a custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,32 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 191, 51, 215),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-               Colors.blue,
-               Colors.purple,
-               Colors.pink,
-              ],
-              begin : Alignment.topLeft,
-              end: Alignment.bottomLeft
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!', style: TextStyle(
-              color: Colors.amber, 
-              fontSize: 28.0,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Colors.blue,
+          Colors.purple,
+          Colors.pink,
+        ], begin: Alignment.topLeft, end: Alignment.bottomLeft),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.amber,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
