bbbb917a59ba1a3555100bbd4671c47654d2d128
Karan <karankhandekar028@gmail.com>
2024-10-30 23:09:55 +0530

    Added Button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -12,14 +12,31 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {}
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2], begin: startAlignment, end: endAlignment),
+          colors: [color1, color2], 
+          begin: startAlignment, 
+          end: endAlignment
+        ),
       ),
-      child: Center(child: Image.asset('assets/images/dice-1.png', width: 200)),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png', 
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDIce,
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
