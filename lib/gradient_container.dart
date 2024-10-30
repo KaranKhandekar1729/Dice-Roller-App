60e0fb6a437ca8b284c918ab4054cac24693b324
Karan <karankhandekar028@gmail.com>
2024-10-30 23:26:10 +0530

    Funcions, styled buttons and worked with paddings

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,13 +25,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-1.png', 
               width: 200,
             ),
+            const SizedBox(height: 20),
             TextButton(
-              onPressed: rollDIce,
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28.0
+                ),
+              ),
               child: const Text('Roll Dice'),
             ),
           ],
